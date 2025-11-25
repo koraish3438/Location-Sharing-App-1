4aaffd712e36b6b4d133fbf81a3e5d87c54482f5
koraish3438 <koraishmdkajamall38@gmail.com>
2025-11-25 09:37:51 +0600

    6th commit

--- app/src/main/java/com/example/locationsharingapp1/viewModel/FireStoreViewModel.kt
+++ app/src/main/java/com/example/locationsharingapp1/viewModel/FireStoreViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.locationsharingapp1.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.example.locationsharingapp1.data.User
 import com.google.firebase.firestore.FirebaseFirestore
 
 class FireStoreViewModel: ViewModel() {
@@ -16,14 +17,73 @@ class FireStoreViewModel: ViewModel() {
         )
 
         usersCollection.document(userId).set(user)
-            .addOnSuccessListener {
-
-            }
-            .addOnFailureListener {
-                // Handle failure
-            }
-
-
+            .addOnSuccessListener { }
+            .addOnFailureListener {e->}
     }
 
+    fun getAllUser(callback: (List<User>) -> Unit) {
+        usersCollection.get()
+            .addOnSuccessListener { result ->
+                val userList = mutableListOf<User>()
+                for (document in result) {
+                    val userId = document.id
+                    val displayName = document.getString("displayName")?: ""
+                    val email = document.getString("email")?: ""
+                    val location = document.getString("location")?: ""
+
+                    userList.add(User(userId, displayName, email, location))
+                }
+                callback(userList)
+            }
+            .addOnFailureListener { e ->
+                callback(emptyList())
+            }
+    }
+
+    fun updateUser(userId: String,displayName: String, email: String, location: String) {
+        val user = hashMapOf(
+            "displayName" to displayName,
+            "location" to location
+        )
+
+        val userMap = user.toMap()
+        usersCollection.document(userId).update(userMap)
+            .addOnSuccessListener {  }
+            .addOnFailureListener {  }
+    }
+
+    fun updateUserLocation(userId: String, location: String) {
+        if(userId.isEmpty()) {
+            return
+        }
+        val user = hashMapOf(
+            "location" to location
+        )
+        val userMap = user.toMap()
+        usersCollection.document(userId).update(userMap)
+            .addOnSuccessListener {  }
+            .addOnFailureListener {  }
+    }
+
+    fun getUser(userId: String, callback: (User?) -> Unit) {
+        usersCollection.document(userId).get()
+            .addOnSuccessListener { documentSnapshot ->
+                val user = documentSnapshot.toObject(User::class.java)
+                callback(user)
+            }
+            .addOnFailureListener {
+                callback(null)
+            }
+    }
+
+    fun getUserLocation(userId: String, callback: (String) -> Unit) {
+        usersCollection.document(userId).get()
+            .addOnSuccessListener { documentSnapshot ->
+                val location = documentSnapshot.getString("location") ?: ""
+                callback(location)
+            }
+            .addOnFailureListener {
+                callback("")
+            }
+    }
 }
