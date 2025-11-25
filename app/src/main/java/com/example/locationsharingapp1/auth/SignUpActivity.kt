ed2b638e411af6d190cd2bd168f0cfdf617e073d
koraish3438 <koraishmdkajamall38@gmail.com>
2025-11-25 16:35:36 +0600

    8th commit

--- app/src/main/java/com/example/locationsharingapp1/auth/SignUpActivity.kt
+++ app/src/main/java/com/example/locationsharingapp1/auth/SignUpActivity.kt
@@ -1,11 +1,16 @@
 package com.example.locationsharingapp1.auth
 
+import android.content.Intent
 import com.example.locationsharingapp1.databinding.ActivitySignUpBinding
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.locationsharingapp1.viewModel.AuthenticationViewModel
 import com.example.locationsharingapp1.viewModel.FireStoreViewModel
+import com.example.locationsharingapp1.welcome.FriendListActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
@@ -23,13 +28,41 @@ class SignUpActivity : AppCompatActivity() {
         fireStoreViewModel = ViewModelProvider(this)[FireStoreViewModel::class.java]
 
         binding.btnSignUp.setOnClickListener {
-            val firstName = binding.etFirstName.text.toString()
-            val lastName = binding.etLastName.text.toString()
             val email = binding.etEmailSignUp.text.toString()
             val password = binding.etPasswordSignUp.text.toString()
-            val confirmPassword = binding.etConfirmPassword.text.toString()
 
+            authViewModel.signUp(
+                email,
+                password,
+                onSuccess = {
+                    val displayName =
+                        binding.etFirstName.text.toString() + " " + binding.etLastName.text.toString()
+                    val location = "Location not available"
+                    fireStoreViewModel.saveUser(
+                        authViewModel.getCurrentUserId(),
+                        displayName,
+                        email,
+                        location
+                    )
+                    startActivity(Intent(this, FriendListActivity::class.java))
+                    finish()
+                },
+                onFailure = { errorMassage ->
+                    Toast.makeText(this, errorMassage, Toast.LENGTH_SHORT).show()
+                }
+            )
+        }
 
+        binding.tvGoToSignIn.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(Firebase.auth.currentUser != null) {
+            startActivity(Intent(this, FriendListActivity::class.java))
+            finish()
         }
     }
 }
